a62effc3a42a380d91ef9abd72718c9543f30802
emericgautier <emericgtr@gmail.com>
2022-08-28 15:27:06 +0200

    app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app-container">
+      <h1>React Cooking App</h1>
+      <input type="text" placeholder="Tapez le nom d'un aliment" />
+      <div className="meals-container"></div>
+    </div>
+  );
 };
 
 export default App;
