15e1184364cd8506113ae1fa4b93294107aecca7
emericgautier <emericgtr@gmail.com>
2022-08-28 21:56:47 +0200

    display data using .map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Card from "./components/Card";
 
 const App = () => {
   const [mealsData, setMealsData] = useState([]);
@@ -15,6 +16,9 @@ const App = () => {
       <h1>React Cooking App</h1>
       <input type="text" placeholder="Tapez le nom d'un aliment" />
       <div className="meals-container"></div>
+      {mealsData.map((meal) => (
+        <Card key={meal.idMeal} meal={meal} />
+      ))}
     </div>
   );
 };
